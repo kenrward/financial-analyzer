9d47baa4f4fd12ff550f1b29526ce69373992176
kenrward <kenrward@gmail.com>
2025-07-14 16:48:34 -0400

    simplify

--- tda/download_aggregates.py
+++ tda/download_aggregates.py
@@ -1,6 +1,6 @@
 import os
 import boto3
-from botocore.exceptions import ClientError
+from botocore.config import Config
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
@@ -11,6 +11,30 @@ load_dotenv()
 # Your Polygon.io S3 Credentials
 ACCESS_KEY = os.getenv("POLYGON_ACCESS_KEY")
 SECRET_KEY = os.getenv("POLYGON_SECRET_KEY")
+
+session = boto3.Session(
+  aws_access_key_id=ACCESS_KEY ,
+  aws_secret_access_key=SECRET_KEY ,
+)
+
+# Create a client with your session and specify the endpoint
+s3 = session.client(
+  's3',
+  endpoint_url='https://files.polygon.io',
+  config=Config(signature_version='s3v4'),
+)
+
+# List Example
+# Initialize a paginator for listing objects
+paginator = s3.get_paginator('list_objects_v2')
+
+# Choose the appropriate prefix depending on the data you need:
+# - 'global_crypto' for global cryptocurrency data
+# - 'global_forex' for global forex data
+# - 'us_indices' for US indices data
+# - 'us_options_opra' for US options (OPRA) data
+# - 'us_stocks_sip' for US stocks (SIP) data
+prefix = 'us_stocks_sip'  # Example: Change this prefix to match your data need
 
 # S3 Bucket Details
 BUCKET_NAME = "flatfiles"
@@ -35,17 +59,7 @@ def download_polygon_data():
     # Create the destination directory if it doesn't exist
     os.makedirs(DESTINATION_DIR, exist_ok=True)
 
-    # Initialize S3 client
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY,
-        endpoint_url=ENDPOINT_URL,
-        region_name="us-east-1" # This can typically be a default value
-    )
-    
-    print("Successfully connected to Polygon.io S3 storage.")
-    
+
     # Calculate date range (last 180 days)
     end_date = datetime.now() - timedelta(days=1)
     start_date = end_date - timedelta(days=2)
@@ -72,18 +86,10 @@ def download_polygon_data():
             
         print(f"Attempting to download: {s3_key}")
 
-        try:
-            # Download the file
-            s3_client.download_file(BUCKET_NAME, s3_key, local_filepath)
-            print(f"-> Successfully downloaded to {local_filepath}")
         
-        except ClientError as e:
-            if e.response['Error']['Code'] == '404':
-                # Files for weekends or market holidays won't exist
-                print(f"-> File not found. Likely a weekend or holiday.")
-            else:
-                print(f"-> An error occurred: {e}")
-
+        s3.download_file(BUCKET_NAME, s3_key, local_filepath)
+        print(f"-> Successfully downloaded to {local_filepath}")
+        
         # Move to the next day
         current_date += timedelta(days=1)
 
